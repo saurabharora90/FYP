8145f6ee65a1a34c48f46ec1fb0095d86d50360a
Saurabh <saurabharora90@gmail.com>
2012-12-30 02:18:30 +0530

    Added WiFly Test on Mega Board

---
+++ Arduino Programs/WiFly_Test_Mega/Credentials.h
@@ -0,0 +1,9 @@
+#ifndef __CREDENTIALS_H__
+#define __CREDENTIALS_H__
+
+// Wifi parameters
+char passphrase[] = "thefriendlyghost";
+char ssid[] = "boo";
+char ntp_server[] = "nist1-la.ustiming.org"; // or your favorite ntp server.
+
+#endif
